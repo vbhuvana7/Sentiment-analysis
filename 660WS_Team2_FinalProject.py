9297a372e09c1cf9b2fe5770c8c5288b0e3fdd4b
Bhuvana Vellanki <55559185+vbhuvana7@users.noreply.github.com>
2020-06-04 16:16:50 -0400

    Add files via upload

---
+++ 660WS_Team2_FinalProject.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+
+### BIA 660WS_Final Project_Team2: Names: Saroja Kondamudi, Bhuvana Vellanki, Ishan Desai
+
+
+from sklearn.metrics import accuracy_score
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from pathlib import Path
+
+#defining the function which takes filename as input and returns a list of reviews and labels
+def loadData(filename):
+    fname=Path(filename)
+    reviews=[]
+    labels=[]
+    f=open(fname,encoding = 'utf-8')
+    for line in f:
+        if len(line.strip().rsplit('\t',1))==2:
+            review,rating=line.strip().rsplit('\t',1)
+            reviews.append(review.lower())    
+            labels.append(int(rating))
+    f.close()
+    return reviews,labels
+
+#defining the function which takes filename as input and returns a list of reviews
+def loadUnlabelData(filename):
+    fname=Path(filename)
+    reviews=[]
+    f=open(fname,encoding = 'utf-8')
+    for line in f:
+        review=line.strip()
+        reviews.append(review.lower())
+    f.close()
+    return reviews
+
+# Give the path for the training data 
+rev_train,labels_train= loadData("train_final.txt")
+
+# Give the path for the testing data 
+rev_test=loadUnlabelData("test_final.txt")
+
+# Logistic Regression and tfidf pipeline
+tvc_pipe = Pipeline([
+ ('tvec', TfidfVectorizer(max_df=0.6,sublinear_tf=True)),
+ ('logreg', LogisticRegression())
+])
+
+tvc_pipe.fit(rev_train, labels_train)
+
+# printing the predicted labels
+predicted= tvc_pipe.predict(rev_test)
+np.savetxt('predicted_labels.txt',predicted,fmt="%d")                          
+
